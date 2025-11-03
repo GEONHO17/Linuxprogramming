bbb2c733b355eed2d6aa52be46bdbf31fe09e59e
GEONHO17 <skyy4035@gmail.com>
2025-11-03 21:34:30 +0900

    Update Mylib.h

--- 7장-1/실습과제1/Mylib.h
+++ 7장-1/실습과제1/Mylib.h
@@ -1,13 +1,13 @@
-#ifndef _MYLIB_H
+#ifndef _MYLIB_H     //중복 방지 
 #define _MYLIB_H
 
-class Complex{
-    int x, y;
-public:
-    Complex(int a=0,int b=0);
-    void show();
-    friend Complex ComplexAdd(Complex a1,Complex a2);
-};
+class Complex{        //클래스 정의
+    int x, y;         //실수부 허수부를 저장할 변수선언 
+public:               //접근 지정자 
+    Complex(int a=0,int b=0);    //생성자
+    void show();       //복소수 출력 함수 
+    friend Complex ComplexAdd(Complex a1,Complex a2);    //복소수를 더하는 함수
+};                     //클래스 끝 
 
-#endif
+#endif                //_MYLIB_H 
 
