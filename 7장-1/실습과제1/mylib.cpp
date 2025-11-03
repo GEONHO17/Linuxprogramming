0610f7ea9ae719057fbfbe557c20f34dbfafc243
GEONHO17 <skyy4035@gmail.com>
2025-11-03 20:12:37 +0900

    Update mylib.cpp

--- 7장-1/실습과제1/mylib.cpp
+++ 7장-1/실습과제1/mylib.cpp
@@ -1,1 +1,16 @@
+#include "Mylib.h"
+#include <iostream>
+using namespace std;
 
+Complex::Complex(int a,int b){
+    x = a; y = b;
+    cout <<"복소수 "<<x<<"+"<<y<<"j 생성"<<endl;
+}
+void Complex::show(){
+    cout << x << "+" <<y <<"j"<<endl;
+}
+Complex ComplexAdd(Complex a1,Complex a2){
+    int real = a1.x + a2.x; 
+    int img =  a1.y + a2.y;
+    return Complex(real,img);
+}
