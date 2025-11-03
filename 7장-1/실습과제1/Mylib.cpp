f5fc19d593d0e06c0d42f6f31b4975cba0e0aea8
GEONHO17 <skyy4035@gmail.com>
2025-11-03 21:36:50 +0900

    Update Mylib.cpp

--- 7장-1/실습과제1/Mylib.cpp
+++ 7장-1/실습과제1/Mylib.cpp
@@ -1,16 +1,16 @@
-#include "Mylib.h"
-#include <iostream>
-using namespace std;
+#include "Mylib.h"    //헤더파일 선언 
+#include <iostream>    //입출력을 제공하는 헤더파일 포함 
+using namespace std;   //네임스페이스 이름공간 생략 
 
-Complex::Complex(int a,int b){
-    x = a; y = b;
-    cout <<"복소수 "<<x<<"+"<<y<<"j 생성"<<endl;
-}
-void Complex::show(){
-    cout << x << "+" <<y <<"j"<<endl;
-}
-Complex ComplexAdd(Complex a1,Complex a2){
-    int real = a1.x + a2.x; 
-    int img =  a1.y + a2.y;
-    return Complex(real,img);
-}
+Complex::Complex(int a,int b){    //생성자 정의 
+    x = a; y = b;                 //입력받은 값으로 초기화 
+    cout <<"복소수 "<<x<<"+"<<y<<"j 생성"<<endl;  //메시지 출력 
+}                                 //생성자 끝 
+void Complex::show(){             //복소수 출력 함수 
+    cout << x << "+" <<y <<"j"<<endl;    //메시지 출력 
+}                                 //함수 끝 
+Complex ComplexAdd(Complex a1,Complex a2){    //두개의 복소수를 더하는 함수 
+    int real = a1.x + a2.x;                   //실수부 더하기 
+    int img =  a1.y + a2.y;                   //허수부 더하기 
+    return Complex(real,img);                 //실수부 허수부 반환 
+}                                            //함수 끝 
