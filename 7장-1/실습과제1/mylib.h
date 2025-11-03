80f3f946b5d567cd84ef4d1395b2adbd43128a2a
GEONHO17 <skyy4035@gmail.com>
2025-11-03 20:12:54 +0900

    Update mylib.h

--- 7장-1/실습과제1/mylib.h
+++ 7장-1/실습과제1/mylib.h
@@ -1,1 +1,13 @@
+#ifndef _MYLIB_H
+#define _MYLIB_H
+
+class Complex{
+    int x, y;
+public:
+    Complex(int a=0,int b=0);
+    void show();
+    friend Complex ComplexAdd(Complex a1,Complex a2);
+};
+
+#endif
 
