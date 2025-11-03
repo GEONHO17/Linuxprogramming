883275eb16865468b4fe3a1de4f4e8764357840d
GEONHO17 <skyy4035@gmail.com>
2025-11-03 21:30:36 +0900

    Update main.cpp

--- 7장-1/실습과제1/main.cpp
+++ 7장-1/실습과제1/main.cpp
@@ -1,9 +1,9 @@
-#include "Mylib.h"
-#include <iostream>
-using namespace std;
-int main(){
-    Complex x(2,3),y(5,-10),sum;
-    sum = ComplexAdd(x,y);
-    sum.show();
-    return 0;
-}
+#include "Mylib.h"    //헤더 파일 선언 
+using namespace std;  //네임스페이스 이름 생략 
+int main(){            //메인함수 
+    Complex x(2,3),y(5,-10),sum;    //클래스 객체 생성 
+    sum = ComplexAdd(x,y);        //클래스 객체에 대입 
+    cout <<"두 복소수의 합은";    //메시지 출력  
+    sum.show();            //출력 
+    return 0;              //0반환후 함수 종료 
+}                          //함수 끝 
