2e0c26201cacf6acf860f362aaf22481a3f6b113
GEONHO17 <skyy4035@gmail.com>
2025-11-17 22:10:33 +0900

    Update mylib.cpp

--- 8장-2/실습과제3/mylib.cpp
+++ 8장-2/실습과제3/mylib.cpp
@@ -1,6 +1,6 @@
 #include "mylib.hpp"            //헤더 파일 포함 
 
-void Shape::Shape(int x,int y){  //멤버 변수 설정함수    
+Shape::Shape(int x,int y){  //멤버 변수 설정함수    
     this -> x = x;              //입력받은 값 x로 설정
     this -> y = y;              //입력받은 값 y로 설정 
     cout <<"Shape생성, ";       //메시지 출력 
@@ -11,7 +11,7 @@ void Shape::show(){     //출력 함수
 Shape::~Shape(){                        //소멸자 정의
     cout <<"Shape 소멸"<<endl;          //메시지 출력 
 }
-void Circle::Circle(int x,int y,int rad):Shape(x,y){ //멤버 변수 설정함수
+Circle::Circle(int x,int y,int rad):Shape(x,y){ //멤버 변수 설정함수
     this -> rad = rad;                  //입력받은 값 rad로 설정
     cout <<"Circle 생성"<<endl;         //메시지 출력 
 }
@@ -22,7 +22,7 @@ void Circle::show(){                        //출력 함수
 Circle::~Circle(){                  //소멸자 정의
     cout <<"Circle 소멸, ";         //메시지 출력 
 }
-void Rect::Rect(int x,int y,int width,int height):Shape(x,y){  //멤버 변수 설정 함수 
+Rect::Rect(int x,int y,int width,int height):Shape(x,y){  //멤버 변수 설정 함수 
     this -> width = width;      //입력받은 값 width로 설정
     this -> height = height;    //입력받은 값 height 로 설정
     cout <<"Rect 생성"<<endl;   //메시지 출력 
@@ -34,7 +34,7 @@ void Rect::show(){              //출력 함수
 Rect::~Rect(){                  //소멸자 정의
     cout << "Rect 소멸, ";      //메시지 출력 
 }
-void Triangle:: Triangle(int x,int y,int width,int height):Shape(x,y){  //멤버 변수 설정함수
+Triangle:: Triangle(int x,int y,int width,int height):Shape(x,y){  //멤버 변수 설정함수
     this -> width = width;      //입력받은 값 width로 설정
     this -> height = height;    //입력받은 값 height로 설정
     cout <<"Triangle 생성"<<endl;   //메시지 출력 
